1b54610d7ca80ddf910dc9d14a4a61f6cffb69c8
Maxmarra <5919669@gmail.com>
2022-08-01 08:09:01 +0300

    ok

--- app/src/main/java/com/example/openweatherapp/presentation/WeatherForecast.kt
+++ app/src/main/java/com/example/openweatherapp/presentation/WeatherForecast.kt
@@ -3,9 +3,13 @@ package com.example.openweatherapp.presentation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.openweatherapp.presentation.state.WeatherState
 
 @Composable
@@ -15,12 +19,18 @@ fun WeatherForecast(
 
 ) {
     state.weatherData?.let { data ->
-        Spacer(modifier = Modifier.height(16.dp))
         Column(
             modifier = modifier
                 .fillMaxWidth()
                 .padding(horizontal = 3.dp)
         ) {
+            Text(
+                text = "Прогноз на 5 дней",
+                fontSize = 20.sp,
+                color = MaterialTheme.colors.onPrimary,
+            modifier = Modifier.padding(16.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             LazyRow(content = {
                 items(data){
                     DailyWeatherDisplay(
